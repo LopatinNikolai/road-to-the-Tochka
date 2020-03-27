8dcf7f6801b1ad67cd570e4f7311aa1b58f6f6fb
LopatinNikolai <lopatin.nikolai@mail.ru>
2020-03-27 12:15:47 +0500

    v3

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -18,10 +18,12 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private lateinit var searchField: EditText
+    private lateinit var buttonPrevious: Button
     private lateinit var searchButton: Button
     private lateinit var userList : RecyclerView
     private lateinit var usersAdapter: UsersAdapter
     private var users = mutableListOf<UserGit>()
+    private var page :Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -29,6 +31,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         searchField = findViewById(R.id.search_field)
         searchButton = findViewById(R.id.search_button)
+        buttonPrevious = findViewById(R.id.previous_page)
+        buttonPrevious.visibility = View.GONE
         userList = findViewById(R.id.rv_users)
         var layoutManager =  LinearLayoutManager(this)
         userList.layoutManager = layoutManager
@@ -36,22 +40,37 @@ class MainActivity : AppCompatActivity() {
         usersAdapter = UsersAdapter(users, this)
         userList.adapter = usersAdapter
     }
-     fun onClick(view: View,page:Int){
-
-         users.clear()
-       var user = searchField.getText().toString()
-         val repository = SearchRepositoryProvider.provideSearchRepository()
-         repository.searchUsers(user, page)
-             .observeOn(AndroidSchedulers.mainThread())
-             .subscribeOn(Schedulers.io())
-             .subscribe ({
-                     result ->
-                 users.addAll(result.items)
-                 usersAdapter.notifyDataSetChanged()
-             }, { error ->
-                 error.printStackTrace()
-             })
-
+     fun onClick(view: View){
+         page = 1
+         buttonPrevious.visibility = View.GONE
+         search(page);
+    }
+    fun onClickNext(view: View){
+        page+=1
+        buttonPrevious.visibility = View.VISIBLE
+        search(page);
+    }
+    fun onClickPrevious(view: View){
+        page-=1
+        if (page < 2){
+            buttonPrevious.visibility = View.GONE
+        }
+        search(page);
+    }
+    fun search(page: Int){
+        users.clear()
+        var user = searchField.getText().toString()
+        val repository = SearchRepositoryProvider.provideSearchRepository()
+        repository.searchUsers(user, page)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe ({
+                    result ->
+                users.addAll(result.items)
+                usersAdapter.notifyDataSetChanged()
+            }, { error ->
+                error.printStackTrace()
+            })
     }
 
 }
